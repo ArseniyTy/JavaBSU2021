cf5cb89b251a83f404278196d407ce9d570ab06e
Arseniy <a.tyletsky@gmail.com>
2021-11-03 23:43:10 +0300

    Add GroupingCollection

---
+++ lab-03-zhabdex/monitoring/src/main/java/by/zhabdex/monitoring/GroupingCollection.java
@@ -0,0 +1,32 @@
+package by.zhabdex.monitoring;
+
+import java.util.*;
+import java.util.function.Function;
+
+public class GroupingCollection<T, K>
+        implements ProcessedCollection<T, Map.Entry<? extends K, ? extends List<? extends T>>> {
+    private final Function<? super T, ? extends K> classifier;
+    private final HashMap<K, List<T>> keys_with_elements = new HashMap<>();
+
+//    private List<E> mappedElements = new ArrayList<>();
+//    private final HashMap<T, E> buffer = new HashMap<>();
+
+    public GroupingCollection(Function<? super T, ? extends K> classifier) {
+        this.classifier = classifier;
+    }
+
+    @Override
+    public void renew(Collection<? extends T> elements) {
+        elements.forEach(e -> {
+            var key = classifier.apply(e);
+            keys_with_elements.putIfAbsent(key, new ArrayList<>());
+            keys_with_elements.get(key).add(e);
+        });
+    }
+
+    @Override
+    public Collection<? extends Map.Entry<? extends K, ? extends List<? extends T>>> currentState() {
+        return keys_with_elements.entrySet();
+    }
+
+}
---
+++ lab-03-zhabdex/monitoring/src/main/java/by/zhabdex/monitoring/MyAnotherMonitoring.java
@@ -0,0 +1,39 @@
+package by.zhabdex.monitoring;
+
+import by.derovi.service_monitoring.visualizer.Table;
+import by.zhabdex.common.Service;
+import by.zhabdex.monitoring_lib.ActiveMonitoring;
+import by.zhabdex.monitoring_lib.Monitoring;
+
+import java.util.AbstractMap;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+@ActiveMonitoring
+public class MyAnotherMonitoring implements Monitoring {
+    FinalProcessedCollection<Service, Table> collection =
+            new GroupingCollection<>(Service::getDataCenter)
+                    .compose(
+                            new MappedCollection<>(
+                                    entry -> new AbstractMap.SimpleEntry<>(entry.getKey(),
+                                            entry.getValue().stream()
+                                                    .mapToLong(Service::getRequestsPerSecond).sum())
+                            )
+                    ).compose(
+                            new TableViewCollection<>("Summary ping", List.of(
+                                    TableViewCollection.ColumnProvider.of("Name", Map.Entry::getKey),
+                                    TableViewCollection.ColumnProvider.of("Available nodes", Map.Entry::getValue)
+                            ))
+                    );
+
+    @Override
+    public void update(Collection<? extends Service> services) {
+        collection.renew(services);
+    }
+
+    @Override
+    public Table getStatistics() {
+        return collection.currentState();
+    }
+}
