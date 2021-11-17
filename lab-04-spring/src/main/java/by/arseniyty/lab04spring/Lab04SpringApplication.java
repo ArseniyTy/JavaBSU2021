d51af44c4fca2ac8c7452816c6db17650e2a5033
ArseniyTy <a.tyletsky@gmail.com>
2021-11-17 18:19:20 +0300

    Init spring app

---
+++ lab-04-spring/src/main/java/by/arseniyty/lab04spring/Lab04SpringApplication.java
@@ -0,0 +1,13 @@
+package by.arseniyty.lab04spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab04SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab04SpringApplication.class, args);
+    }
+
+}
---
+++ lab-04-spring/src/test/java/by/arseniyty/lab04spring/Lab04SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package by.arseniyty.lab04spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab04SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
